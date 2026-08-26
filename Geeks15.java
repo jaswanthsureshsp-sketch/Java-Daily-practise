ef140a7c0e4fb8a10e71ed95ebfc5400e0e8a861
Jaswanth Suresh <23147109@gmail.com>
2026-08-26 19:28:17 +0530

    Test case 2 incorrect password

--- Geeks15.java
+++ Geeks15.java
@@ -1,8 +1,8 @@
 public class Geeks15 {
     public static void main(String[] agrs){
-        checkLogin("Admin", "Pass123");
+        check("Admin", "Pass");
     }
-    public static void checkLogin(String username, String pass){
+    public static void check(String username, String pass){
         if(username.equals("Admin")){
             if(pass.equals("Pass123")){
                 System.out.println("Login SuccessFull");
